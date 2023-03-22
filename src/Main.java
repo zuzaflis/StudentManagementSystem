70004f1094da80b5dc2409d18995bbc7a2b535a3
zuzaflis <zuzaflis20@gmail.com>
2023-03-22 23:24:26 +0100

    added some changes in addPoints and main

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,11 @@ public class Main {
       Student student = new Student("Karol", " Kawa", StudentCondition.PRESENT, 2022,200);
       List<Student> studentsList = new ArrayList<>();
     //st.add(student);
-    Class klasa = new Class("klaska",studentsList,6);
+    Class klasa = new Class("klasa",studentsList,6);
     klasa.addStudent(student);
     klasa.addPoints(student,30.0);
     klasa.removePoints(student,15.0);
-      klasa.summary();
+      //klasa.summary();
 
 
       ClassContainer classContainer = new ClassContainer();
@@ -18,8 +18,8 @@ public class Main {
         Class grupa1 = classContainer.getGroups().get("klasa1");
 
         grupa1.addStudent(student);
-       // grupa1.summary();
-        //classContainer.summary();
+        grupa1.summary();
+        classContainer.summary();
 
 
     }
